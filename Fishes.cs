7f824a129d425b82d261c401764c3d94ec94017e
Sofia <117852305+Sofia777333@users.noreply.github.com>
2022-11-10 12:20:14 +0600

    Add files via upload

---
+++ Fishes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace Class
+{
+    class Fishes
+    {
+     public string Name;
+     private string _kind;
+     private string _color;
+     private int _size;
+
+     public Fishes() { }
+    public Fishes(string name, string Kind, string color, int size)
+        {
+            Name = name;
+            _kind = Kind;
+            _color = color;
+            _size = size;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Имя: {Name}");
+            Console.WriteLine($"Вид: {_kind}");
+            Console.WriteLine($"Окрас: {_color}");
+            Console.WriteLine($"Размер: {_size}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Class
+{
+class Program
+    {
+   static void Main()
+        {
+            Fishes fishes = new Fishes();
+          
+            fishes.DisplayInfo();
+
+            Fishes anotherfishes = new Fishes();
+           
+            anotherfishes.DisplayInfo();
+        }
+    }
+}
